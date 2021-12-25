d3383e20de91c1ee2f5c2ae2dbd86a3d6f97cddb
kimsoosoo0928 <kimsoosoo0928@gmail.com>
2021-12-26 05:25:07 +0900

    test

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace grade_project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("학사관리 프로그램");
+            Console.WriteLine("");
+            Console.WriteLine("메뉴");
+            Console.WriteLine("");
+            Console.WriteLine("1. 회원가입");
+            Console.WriteLine("2. 로그인");
+            Console.WriteLine("");
+            Console.WriteLine("메뉴 번호를 입력하세요");
+            Console.ReadLine();
+            
+        }
+    }
+}
